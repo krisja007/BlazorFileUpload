45453af18590e54e75efd6fd922da8ffee172022
krisja007 <kongxsxs@gmail.com>
2023-05-27 15:30:22 +0700

    Initial commit

---
+++ BlazorFileUpload/Shared/UploadResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorFileUpload.Shared
+{
+    public class UploadResult
+    {
+        
+        public string? FileName { get; set; }
+        public string? StoredFileName { get; set; }
+        
+    }
+}
